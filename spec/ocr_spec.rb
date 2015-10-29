13cd85dc6e0fbd22dd277257e065bbdc21ff43dd
Jason Lee <huacnlee@gmail.com>
2015-10-29 18:28:11 +0800

    RTesseract use mini_magick.

--- spec/ocr_spec.rb
+++ spec/ocr_spec.rb
@@ -24,7 +24,7 @@ describe 'OCR' do
   it 'should not read by OCR lib' do
     results = []
     @samples.each do |chars|
-      str = RTesseract.new(File.join(File.dirname(__FILE__), "..", "tmp", "#{chars}.png")).to_s
+      str = RTesseract.new(File.join(File.dirname(__FILE__), "..", "tmp", "#{chars}.png"), processor: 'mini_magick').to_s
       results << "- Chars: #{chars}, OCR read #{str.strip}"
       expect(chars).not_to eq(str)
     end
