6498226e5e682a029c5b1fee47b7a730d4af7876
Altherlex Alves <para.alves@gmail.com>
2016-08-08 17:34:46 -0300

    adding option to reload via ajax

--- lib/rucaptcha/view_helpers.rb
+++ lib/rucaptcha/view_helpers.rb
@@ -1,5 +1,11 @@
 module RuCaptcha
   module ViewHelpers
+
+    def reload_ajax_tag(img_captcha_selector)
+      js = %Q( jQuery.getJSON("/rucaptcha", function(json){ jQuery('#{img_captcha_selector}').attr('src', json.data) }) )
+      link_to "Reload!", "javascript:#{js}", type:'button'
+    end
+
     def rucaptcha_input_tag(opts = {})
       opts[:name]           = '_rucaptcha'
       opts[:type]           = 'text'
@@ -11,9 +17,16 @@ module RuCaptcha
       tag(:input, opts)
     end
 
+
     def rucaptcha_image_tag(opts = {})
       opts[:class] = opts[:class] || 'rucaptcha-image'
-      image_tag(ru_captcha.root_url, opts)
+
+      if opts.delete(:reload)
+        image_tag(ru_captcha.root_url, opts)+
+        reload_ajax_tag("##{opts[:id]}"||".#{opts[:class]}")
+      else
+        image_tag(ru_captcha.root_url, opts)
+      end
     end
   end
 end
