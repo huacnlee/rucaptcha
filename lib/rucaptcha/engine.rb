47c5edf2545d623df9585313658268312d81920b
Jason Lee <huacnlee@gmail.com>
2017-03-09 10:17:30 +0800

    Mount engine use `prepend` method to get high priority in routes.

--- lib/rucaptcha/engine.rb
+++ lib/rucaptcha/engine.rb
@@ -4,8 +4,8 @@ module RuCaptcha
 
     initializer 'rucaptcha.init' do |app|
       # https://github.com/rails/rails/blob/3-2-stable/actionpack/lib/action_dispatch/routing/route_set.rb#L268
-      # `app.routes.append` start from Rails 3.2 - 5.0
-      app.routes.append do
+      # `app.routes.prepend` start from Rails 3.2 - 5.0
+      app.routes.prepend do
         mount RuCaptcha::Engine => '/rucaptcha'
       end
 
