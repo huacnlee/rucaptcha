0c16db552e2217a55bc85734e9e8a540c8cbd2fe
Jason Lee <huacnlee@gmail.com>
2017-03-09 16:42:43 +0800

    Do not handle with HEAD method

--- app/controllers/ru_captcha/captcha_controller.rb
+++ app/controllers/ru_captcha/captcha_controller.rb
@@ -1,6 +1,7 @@
 module RuCaptcha
   class CaptchaController < ActionController::Base
     def index
+      return head :ok if request.head?
       headers['Cache-Control'] = 'no-cache, no-store, max-age=0, must-revalidate'
       headers['Pragma'] = 'no-cache'
       data = generate_rucaptcha
