43a221ff82a3a2967acedc7af5827c69696871bf
Jason Lee <huacnlee@gmail.com>
2019-12-20 15:00:23 +0800

    Upgrade Rails and Fix tests warning

--- spec/view_spec.rb
+++ spec/view_spec.rb
@@ -3,7 +3,8 @@ require 'spec_helper'
 describe ActionView::Base do
   describe '.rucaptcha_input_tag' do
     it 'should work' do
-      view = ActionView::Base.new
+      context = ActionView::LookupContext.new([])
+      view = ActionView::Base.new(context)
       expect(view.respond_to?(:rucaptcha_input_tag)).to eq true
     end
   end
