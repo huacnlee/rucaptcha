5c01371daac13d8451272bb2e37e1bdaaceee472
Serhii Khoma <srghma@gmail.com>
2021-03-29 17:43:09 +0300

    feat: force browser to NOT cache the image (even if nginx overrides the Cache-Control header)

--- lib/rucaptcha/view_helpers.rb
+++ lib/rucaptcha/view_helpers.rb
@@ -13,7 +13,7 @@ module RuCaptcha
 
     def rucaptcha_image_tag(opts = {})
       opts[:class] = opts[:class] || "rucaptcha-image"
-      opts[:src] = "#{ru_captcha.root_path}?t=#{DateTime.now.strftime("%Q")}"
+      opts[:src] = "#{ru_captcha.root_path}?t=#{Time.now.strftime("%s%L")}"
       opts[:onclick] = "this.src = '#{ru_captcha.root_path}?t=' + Date.now();"
       tag(:img, opts)
     end
